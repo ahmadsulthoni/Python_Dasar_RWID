89c2e55decb54add484e1db129d006d5bfd1f3e6
ahmad sulthoni <ahmadsulthoni.ext@gmail.com>
2022-01-10 17:50:37 +0700

    Percabangan

--- main.py
+++ main.py
@@ -13,3 +13,9 @@ print("Maka Budi berangkat ke toko")
 print("Dan mulai berbelanja")
 
 # Percabangan
+jumlah_botol_susu = 190
+
+if jumlah_botol_susu > 0:
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli susu")
