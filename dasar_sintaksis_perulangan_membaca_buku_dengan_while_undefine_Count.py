ef9952a8b0cd8dbf1eee27ba3e4427779d019c9b
ANONIM <ahmadsulthoni.ext@gmail.com>
2022-01-12 06:15:42 +0700

    Looping data ke 9

--- dasar_sintaksis_perulangan_membaca_buku_dengan_while_undefine_Count.py
+++ dasar_sintaksis_perulangan_membaca_buku_dengan_while_undefine_Count.py
@@ -3,16 +3,20 @@ Program perulangan membaca buku dengan while
 
 """
 Jumlah_buku = 100
-print('Ibu berkata,"Baca semua bukumu"')
+print('Ibu berkata,"Baca dan pahami semua bukumu"')
 
-Jumlah_buku_yang_sudah_dibaca = 0
-print(f"Jumlah buku yang sudah di baca {Jumlah_buku_yang_sudah_dibaca}")
+Total_jumlah_baca = 0
+Jumlah_buku_yang_sudah_dibaca_dan_di_pahami = 0
+print(f"Jumlah buku yang sudah di baca dan di pahami adalah {Jumlah_buku_yang_sudah_dibaca_dan_di_pahami}")
 
-print("Dengan While")
-while Jumlah_buku_yang_sudah_dibaca < Jumlah_buku:
+print("Dengan While Undefine")
+while Jumlah_buku_yang_sudah_dibaca_dan_di_pahami < Jumlah_buku:
+    Total_jumlah_baca = Total_jumlah_baca + 1
+    if Jumlah_buku_yang_sudah_dibaca_dan_di_pahami == 9:
+        print(f"Buku ke {Jumlah_buku_yang_sudah_dibaca_dan_di_pahami} Belum paham")
+    else:
+        Jumlah_buku_yang_sudah_dibaca_dan_di_pahami = Jumlah_buku_yang_sudah_dibaca_dan_di_pahami + 1
+        print(f"Buku ke {Jumlah_buku_yang_sudah_dibaca_dan_di_pahami} sudah di baca")
 
-    Jumlah_buku_yang_sudah_dibaca = Jumlah_buku_yang_sudah_dibaca + 1
-    print(f"Buku ke {Jumlah_buku_yang_sudah_dibaca} sudah di baca")
-
-print(f"Jumlah buku yang sudah di baca {Jumlah_buku_yang_sudah_dibaca}")
+print(f"Jumlah buku yang sudah di baca {Jumlah_buku_yang_sudah_dibaca_dan_di_pahami}")
 print("Program Selesai")
