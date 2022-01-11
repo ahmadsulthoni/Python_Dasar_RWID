3115211bb84fad8b012bac5390851294c3dc7598
ahmad sulthoni <ahmadsulthoni.ext@gmail.com>
2022-01-11 11:15:21 +0700

    Sintaksis perulangan

---
+++ dasar_sintaksis_perulangan.py
@@ -0,0 +1,15 @@
+"""
+Program perulangan membaca buku
+"""
+
+Jumlah_buku = 100
+print('Ibu berkata,"Baca semua bukumu"')
+
+Jumlah_buku_yang_sudah_dibaca = 0
+print(f"Jumlah buku yang sudah di baca {Jumlah_buku_yang_sudah_dibaca}")
+
+for Jumlah_buku_yang_sudah_dibaca in range(1,Jumlah_buku+1):
+
+    print(f"Buku ke {Jumlah_buku_yang_sudah_dibaca} sudah di baca")
+
+print(f"Jumlah buku yang sudah di baca {Jumlah_buku_yang_sudah_dibaca}")
