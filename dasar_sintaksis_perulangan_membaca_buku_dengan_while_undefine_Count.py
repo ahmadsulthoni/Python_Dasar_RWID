4f11dd51472f9b607734dd151213984a5b46f95d
ANONIM <ahmadsulthoni.ext@gmail.com>
2022-01-12 07:19:27 +0700

    Perulangan menggunakan while dengan jumlah tak terbatas dan ada variable pemberhenti

--- dasar_sintaksis_perulangan_membaca_buku_dengan_while_undefine_Count.py
+++ dasar_sintaksis_perulangan_membaca_buku_dengan_while_undefine_Count.py
@@ -10,7 +10,7 @@ Jumlah_buku_yang_sudah_dibaca_dan_di_pahami = 0
 print(f"Jumlah buku yang sudah di baca dan di pahami adalah {Jumlah_buku_yang_sudah_dibaca_dan_di_pahami}")
 
 print("Dengan While Undefine")
-while Jumlah_buku_yang_sudah_dibaca_dan_di_pahami < Jumlah_buku * 2:
+while Total_jumlah_baca < Jumlah_buku * 2:
     Total_jumlah_baca = Total_jumlah_baca + 1
     if Jumlah_buku_yang_sudah_dibaca_dan_di_pahami == 9:
         print(f"Buku ke {Jumlah_buku_yang_sudah_dibaca_dan_di_pahami + 1} Belum paham")
@@ -19,4 +19,9 @@ while Jumlah_buku_yang_sudah_dibaca_dan_di_pahami < Jumlah_buku * 2:
         print(f"Buku ke {Jumlah_buku_yang_sudah_dibaca_dan_di_pahami} sudah di baca dan di pahami")
 
 print(f"Jumlah buku yang sudah di baca dan di pahami {Jumlah_buku_yang_sudah_dibaca_dan_di_pahami}")
+if Jumlah_buku_yang_sudah_dibaca_dan_di_pahami == Jumlah_buku:
+    print(f'"Buku yang sudah di baca dan di pahami sebanyak {Jumlah_buku_yang_sudah_dibaca_dan_di_pahami} Buku')
+else:
+    print(f'"Tidak Semua buku bisa di pahami, dan yang paham hanya '
+          f'{Jumlah_buku_yang_sudah_dibaca_dan_di_pahami} buku saja')
 print("Program Selesai")
