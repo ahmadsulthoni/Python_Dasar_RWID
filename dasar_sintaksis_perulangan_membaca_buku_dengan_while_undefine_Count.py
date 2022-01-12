285d202e53fb26ecc7b4c573796c9fb8fb1497f7
ANONIM <ahmadsulthoni.ext@gmail.com>
2022-01-12 07:23:41 +0700

    rename in english

--- dasar_sintaksis_perulangan_membaca_buku_dengan_while_undefine_Count.py
+++ dasar_sintaksis_perulangan_membaca_buku_dengan_while_undefine_Count.py
@@ -2,26 +2,26 @@
 Program perulangan membaca buku dengan while
 
 """
-Jumlah_buku = 10
+book_count = 10
 print('Ibu berkata,"Baca dan pahami semua bukumu"')
 
-Total_jumlah_baca = 0
-Jumlah_buku_yang_sudah_dibaca_dan_di_pahami = 0
-print(f"Jumlah buku yang sudah di baca dan di pahami adalah {Jumlah_buku_yang_sudah_dibaca_dan_di_pahami}")
+read_count = 0
+understood_count = 0
+print(f"Jumlah buku yang sudah di baca dan di pahami adalah {understood_count}")
 
 print("Dengan While Undefine")
-while Total_jumlah_baca < Jumlah_buku * 2:
-    Total_jumlah_baca = Total_jumlah_baca + 1
-    if Jumlah_buku_yang_sudah_dibaca_dan_di_pahami == 9:
-        print(f"Buku ke {Jumlah_buku_yang_sudah_dibaca_dan_di_pahami + 1} Belum paham")
+while read_count < book_count * 2:
+    read_count = read_count + 1
+    if understood_count == 9:
+        print(f"Buku ke {understood_count + 1} Belum paham")
     else:
-        Jumlah_buku_yang_sudah_dibaca_dan_di_pahami = Jumlah_buku_yang_sudah_dibaca_dan_di_pahami + 1
-        print(f"Buku ke {Jumlah_buku_yang_sudah_dibaca_dan_di_pahami} sudah di baca dan di pahami")
+        understood_count = understood_count + 1
+        print(f"Buku ke {understood_count} sudah di baca dan di pahami")
 
-print(f"Jumlah buku yang sudah di baca dan di pahami {Jumlah_buku_yang_sudah_dibaca_dan_di_pahami}")
-if Jumlah_buku_yang_sudah_dibaca_dan_di_pahami == Jumlah_buku:
-    print(f'"Buku yang sudah di baca dan di pahami sebanyak {Jumlah_buku_yang_sudah_dibaca_dan_di_pahami} Buku')
+print(f"Jumlah buku yang sudah di baca dan di pahami {understood_count}")
+if understood_count == book_count:
+    print(f'"Buku yang sudah di baca dan di pahami sebanyak {understood_count} Buku')
 else:
     print(f'"Tidak Semua buku bisa di pahami, dan yang paham hanya '
-          f'{Jumlah_buku_yang_sudah_dibaca_dan_di_pahami} buku saja')
+          f'{understood_count} buku saja')
 print("Program Selesai")
