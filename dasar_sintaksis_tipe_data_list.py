5357ab3e26d2eeebbba97894f207053acb7ee2ff
ahmad sulthoni <ahmadsulthoni.ext@gmail.com>
2022-01-18 16:19:49 +0700

    perintah del dengan list comprehension start

--- dasar_sintaksis_tipe_data_list.py
+++ dasar_sintaksis_tipe_data_list.py
@@ -68,3 +68,9 @@ daftar_buku = ['Seven Habits', 'How to influence people', 'first thing first','4
 del daftar_buku[:] #del dengan : adalah menghapus semua element yang ada dalam list
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
+
+print('\nperintah del dengan list comprehension start')
+daftar_buku = ['Seven Habits', 'How to influence people', 'first thing first','4DX']
+del daftar_buku[0:1] #del dengan star 0 dan akhiran element 1 yang di hapus
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
